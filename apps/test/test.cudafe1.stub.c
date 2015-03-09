982f35463107aaaf0e7b1cf7f6e2e566a5bfeb55
Taichirou Suzuki <t.suzuki.accept@gmail.com>
2015-03-09 18:08:06 +0900

    20150309

--- apps/test/test.cudafe1.stub.c
+++
@@ -1,7 +0,0 @@
-#define __NV_CUBIN_HANDLE_STORAGE__ static
-#include "crt/host_runtime.h"
-#include "test.fatbin.c"
-static void __nv_cudaEntityRegisterCallback(void **);
-static void __sti____cudaRegisterAll_12_test_cpp1_ii_main(void) __attribute__((__constructor__));
-static void __nv_cudaEntityRegisterCallback(void **__T20){__nv_dummy_param_ref(__T20);}
-static void __sti____cudaRegisterAll_12_test_cpp1_ii_main(void){__cudaRegisterBinary(__nv_cudaEntityRegisterCallback);}
